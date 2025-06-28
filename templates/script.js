0c3e95257d882ad2fe01101278ce85141d22d7d1
simplifiedrobloxapi <spukz14@gmail.com>
2025-06-28 10:44:44 +0200

    Add files via upload

---
+++ templates/script.js
@@ -0,0 +1,9 @@
+function toggleCategory(category) {
+    var element = document.getElementById(category);
+    element.style.display = (element.style.display === "none") ? "block" : "none";
+}
+
+function toggleEndpoint(endpoint) {
+    var element = document.getElementById(endpoint);
+    element.style.display = (element.style.display === "none") ? "block" : "none";
+}
